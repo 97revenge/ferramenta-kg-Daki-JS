1f5cb5bc64d6553c5f6e8354b95f90860044e8aa
Matheus Pereira <empregos97@gmail.com>
2022-05-31 13:49:08 -0300

    null : add fork & primitive code

---
+++ script.js
@@ -0,0 +1,86 @@
+const bagRider = {
+    kg,
+    qnt
+};
+const carrinho = value(0);
+
+
+
+
+const hortifruttiClass = {
+    "batata doce ": {
+        kg: 500,
+        qnt: "2 a 3 UNID",
+        value: 5.99
+    },
+    "tomate saladete": {
+        kg: 500,
+        qnt: "3 a 4 UNID",
+        value: 5.94
+    },
+    "beterraba": {
+        kg: 500,
+        qnt: "3 a 5 UNID",
+        value: 7.99
+
+    },
+    "Pepino Comum": {
+        kg: 500,
+        qnt: "3 a 6 UNID",
+        value: 3.99
+
+    },
+    "cenoura": {
+        kg: 500,
+        qnt: "3 a 6 UNID",
+        value: 8.09
+    },
+    "Mini cebola Bandeja": {
+        kg: 500,
+        qnt: "3 a 6 UNID",
+        value: 3.99
+    }
+
+
+};
+
+
+
+/* GLOBAL CLASSES  */
+const acougueClass = {};
+const congeladosClass = {};
+const padariaClass = {};
+const laticinioClass = {};
+const destiladosClass = {};
+const bebidasClass = {};
+
+
+
+/* GLOBAL CATEGORIES  */
+hortifruttiClass = hort;
+congeladosClass = cong;
+padariaClass = pada;
+laticinioClass = lati;
+destiladosClass = dest;
+bebidasClass = bebi;
+
+
+
+function dropinBag(bagRider, kg, carrinho) {
+
+    switch (bagRider) {
+        case 1:
+            low["50%", 50, "nao esta cheio"]
+        case 2:
+            mid["70%", 70]
+        case 3:
+            high["100%", 100, "está cheia"]
+        default:
+            ["vazia", null, ""]
+    }
+    return bagRider();
+
+
+
+
+}

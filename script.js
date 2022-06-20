318fe65a58e19725d02a65996a81862dcb1dd99a
Matheus Pereira <empregos97@gmail.com>
2022-06-20 12:48:46 -0300

    docs: att hotfix 20/06

--- script.js
+++ script.js
@@ -2,19 +2,18 @@
     kg,
     qnt
 }; */
-Object.create(bagRider);
-
-const bagRider = () => kg,
-    qnt;
 
 
+// const bagRider= (kg, qnt) =>  ;
 
-const carrinho = value(0);
+
+
+const carrinho = () => value(0);
 
 
 /* REFRIGERAÇÃO */
 
-let congelado = () => congelado ? true : false;
+let congelado = () => e ? true : false;
 
 
 /* REFRIGERAÇÃO */
@@ -24,45 +23,33 @@ let congelado = () => congelado ? true : false;
 
 
 
-const hortifruttiClass = (kg, qnt, value) => { "batata doce", kg }
+// const batataDoce = () => { produto:hortifruttiClass["batata-doce "] ,  hortifruttiClass.[{$kg}], hortifruttiClass.qnt[{$qnt}]}
+const hort = (hortifruttiClass) => {
+    hortifruttiClass = {
+        "batata-doce": { kg: 500, qnt: "2 a 3 UNID", value: 5.99 },
+        "tomate saladete": { kg: 500, qnt: "3 a 4 UNID", value: 5.94 },
+        "beterraba": { kg: 500, qnt: "3 a 5 UNID", value: 7.99 },
+        "Pepino Comum": { kg: 500, qnt: "3 a 6 UNID", value: 3.99 },
+        "cenoura": { kg: 500, qnt: "3 a 6 UNID", value: 8.09 },
+        "Mini cebola Bandeja": { kg: 500, qnt: "3 a 6 UNID", value: 3.99 },
+    };
+    return hortifruttiClass.lenght();
 
-const horttifruttiClass = {
-    "batata doce ": {
-        kg: 500,
-        qnt: "2 a 3 UNID",
-        value: 5.99
-    },
-    "tomate saladete": {
-        kg: 500,
-        qnt: "3 a 4 UNID",
-        value: 5.94
-    },
-    "beterraba": {
-        kg: 500,
-        qnt: "3 a 5 UNID",
-        value: 7.99
+}
 
-    },
-    "Pepino Comum": {
-        kg: 500,
-        qnt: "3 a 6 UNID",
-        value: 3.99
-
-    },
-    "cenoura": {
-        kg: 500,
-        qnt: "3 a 6 UNID",
-        value: 8.09
-    },
-    "Mini cebola Bandeja": {
-        kg: 500,
-        qnt: "3 a 6 UNID",
-        value: 3.99
-    }
+var list = hort.hortifruttiClass.filter(function(item) {
+    return (item[0].kg.hort == 500 && item[0].qnt.hort && item[0].value.hort)
 
 
 
-};
+});
+
+
+
+
+
+
+
 const acougueClass = {
 
     frangos: {
@@ -134,7 +121,7 @@ const bebidasClass = {};
 
 
 /* GLOBAL CATEGORIES  */
-hortifruttiClass = hort;
+
 congeladosClass = cong;
 padariaClass = pada;
 laticinioClass = lati;
@@ -157,26 +144,30 @@ function dropinBag(bagRider, kg, carrinho) {
             ["vazia", null, ""]
     }
     return bagRider();
-
-
-
-
 }
 
 function addingBag(bagRider, kg) {};
 
-function calculateKg(carrinho, kg) {
-    if (carrinho <= kg(30000)) {
-        console.log("pesado demais para nossos Riders Levarem!!!")
-    } else if (carrinho <= kg(2000)) {
-        console.log("está bem pesado, nossos riders podem demorar um pouco para chegar no seu destino. quer rever algo?");
-        return carrinho;
-    } else()
+// function calculateKg(carrinho, kg) { // AGUARDANDO PULL REQUEST  
+//     if (carrinho <= kg(30000)) {
+//         console.log("pesado demais para nossos Riders Levarem!!!")
+//     } else if (carrinho <= kg(2000)) {
+//         console.log("está bem pesado, nossos riders podem demorar um pouco para chegar no seu destino. quer rever algo?");
+//         return carrinho;
+//     } 
 
+// } 
+
+
+function calculateKg(carrinho, kg, carrinho) {
+    carrinho <= kg(30000) ?
+        console.log("pesado demais para nossos Riders Levarem!!!") :
+        console.log("parece que esta pesado... nossos riders podem demorar um pouco para chegar no seu destino. quer rever algo?")
+
+    return value;
+
+    ;
 }
-
-
-
 
 
 /*
@@ -184,6 +175,18 @@ ANOTAÇOES DO CODIGO DIA 31/05
 "serão mais ou menos 6 itens de cada categoria no momento. Valor, Peso e Quantidade atualizados. 
 "
 "é necessario criar uma conexao entre calculateKg e DropinBag".
+
+
+ANOTAÇÕES DO CODIGO DIA 19/06
+ "Trabalhei um pouco mais no codigo ... o importante é fazer aconteçer 
+ aplicar variaveis novas e refatorar o codigo sao duas ferramentas que me ajudam a manter o conceito produtivo e ativo , 
+ mesmo que eu nao mexo no projeto a mais de 15 dias ...
+
+
+
+ 
+ "
+
 
 
 */
